26fa987cca1e85df43f6908b155decacd4d9d63f
April Long <april11dance@gmail.com>
2019-09-22 20:14:56 -0600

    starting javascript

---
+++ assets/javascript/app.js
@@ -0,0 +1,42 @@
+$(document).ready(function(){
+
+//Initialize Firebase//
+var firebaseConfig = {
+    apiKey: "AIzaSyBi-O17PsVdUQGHlfY3qhoiIBpnbuKWpM4",
+    authDomain: "test-7d555.firebaseapp.com",
+    databaseURL: "https://test-7d555.firebaseio.com",
+    projectId: "test-7d555",
+    storageBucket: "",
+    messagingSenderId: "98104578426",
+    appId: "1:98104578426:web:c2908835db8c009512c6c3"
+  };
+ 
+  firebase.initializeApp(firebaseConfig);
+
+  //global variable//
+  var currentTime 
+  var firstTrain
+  var trainFrequency 
+  var timeDiff
+  var timeRemaining
+  var nextTrain
+  var timeUntilNextTrain
+
+  
+
+
+  //local variable//
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
